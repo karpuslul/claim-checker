1a5820a886de8822f486de5b2a79ce54c9914b80
sergejgrisanin <nigel-92@mail.ru>
2022-11-18 11:45:01 +0500

    Initial commit

---
+++ src/main/java/ru/innotech/claimchecker/ClaimCheckerApplication.java
@@ -0,0 +1,13 @@
+package ru.innotech.claimchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/innotech/claimchecker/ClaimCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.innotech.claimchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
